5e84827bb9493f4ca731f05452a08f02810df780
sbarnes14 <sbarnes2015@gmail.com>
2021-06-28 15:15:33 -0400

    Add edit and delete functionality

--- HomeServices.Services/PropertyService.cs
+++ HomeServices.Services/PropertyService.cs
@@ -55,5 +55,57 @@ namespace HomeServices.Services
                 return query.ToArray();
             }
         }
+
+        public PropertyDetail GetPropertyById(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Properties
+                        .Single(e => e.PropertyId == id && e.OwnerId == _userId);
+                return
+                    new PropertyDetail
+                    {
+                        PropertyId = entity.PropertyId,
+                        SquareFootage = entity.SquareFootage,
+                        YardSize = entity.Yardsize,
+                        Address = entity.Address
+                    };
+            }
+        }
+
+        public bool UpdateProperty(PropertyEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Properties
+                        .Single(e => e.PropertyId == model.PropertyId && e.OwnerId == _userId);
+
+                entity.PropertyId = model.PropertyId;
+                entity.SquareFootage = model.SquareFootage;
+                entity.Yardsize = model.YardSize;
+                entity.Address = model.Address;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public bool DeleteProperty(int propertyId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        .Properties
+                        .Single(e => e.PropertyId == propertyId && e.OwnerId == _userId);
+
+                ctx.Properties.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
     }
 }
--- HomeServices/Controllers/PropertyController.cs
+++ HomeServices/Controllers/PropertyController.cs
@@ -31,15 +31,96 @@ namespace HomeServices.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(PropertyCreate model)
         {
-            if (!ModelState.IsValid)
+            if (!ModelState.IsValid) return View(model);
+
+            var service = CreatePropertyService();
+
+            if (service.CreateProperty(model))
             {
+                TempData["SaveResult"] = "Your Property was added successfully";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Property could not be added");
+
+            return View(model);
+        }
+
+        private PropertyService CreatePropertyService()
+        {
+            var userId = Guid.Parse(User.Identity.GetUserId());
+            var service = new PropertyService(userId);
+            return service;
+        }
+
+        public ActionResult Details(int id)
+        {
+            var svc = CreatePropertyService();
+            ;
+            var model = svc.GetPropertyById(id);
+
+            return View(model);
+        }
+
+        public ActionResult Edit(int id)
+        {
+            var service = CreatePropertyService();
+            var detail = service.GetPropertyById(id);
+            var model =
+                new PropertyEdit
+                {
+                    PropertyId = detail.PropertyId,
+                    SquareFootage = detail.SquareFootage,
+                    YardSize = detail.YardSize,
+                    Address = detail.Address
+                };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, PropertyEdit model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            if(model.PropertyId != id)
+            {
+                ModelState.AddModelError("", "Id Mismatch");
                 return View(model);
             }
 
-            var userId = Guid.Parse(User.Identity.GetUserId());
-            var service = new PropertyService(userId);
+            var service = CreatePropertyService();
 
-            service.CreateProperty(model);
+            if (service.UpdateProperty(model))
+            {
+                TempData["SaveResult"] = "your property was updated";
+                return RedirectToAction("Index");
+            }
+
+            ModelState.AddModelError("", "Your property could not be updated");
+            return View(model);
+        }
+
+        [ActionName("Delete")]
+        public ActionResult Delete(int id)
+        {
+            var svc = CreatePropertyService();
+            ;
+            var model = svc.GetPropertyById(id);
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteProperty(int id)
+        {
+            var service = CreatePropertyService();
+
+            service.DeleteProperty(id);
+
+            TempData["SaveResult"] = "Your Property was deleted";
 
             return RedirectToAction("Index");
         }
